dd6f4d6887088829d0c019e156f76cb18a0773ae
sandesh362 <sandeshbramahne.362@gmail.com>
2026-09-11 22:04:47 +0530

    updates

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,6 @@
 module.exports = function (api) {
   api.cache(true);
-  return { presets: ['babel-preset-expo'], plugins: ['nativewind/babel'] };
+  return {
+    presets: [['babel-preset-expo', { jsxImportSource: 'nativewind' }], 'nativewind/babel'],
+  };
 };
